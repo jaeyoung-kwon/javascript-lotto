adeaf4f241d106799e96e664d61b71072e3d9097
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2025-02-26 20:52:05 +0900

    feat: modal을 open, close 하는 기능으로 분리

--- src/step2-index.js
+++ src/step2-index.js
@@ -1,4 +1,75 @@
-/**
- * step 2의 시작점이 되는 파일입니다.
- * 노드 환경에서 사용하는 readline 등을 불러올 경우 정상적으로 빌드할 수 없습니다.
- */
+const resultButton = document.getElementById("resultButton");
+
+resultButton.addEventListener("click", () => {
+  if (document.querySelector(".modal_backdrop")) return;
+
+  const modalBackdrop = document.createElement("div");
+  modalBackdrop.className = "modal_backdrop";
+  document.body.appendChild(modalBackdrop);
+
+  modalBackdrop.innerHTML = `
+        <div class="modal_container">
+        <div class="modal_close_button_wrapper">
+            <button class="modal_close_button" id="modalCloseButton">
+              <img src="/icon/close.svg" alt="Close" />
+            </button>
+          </div>
+          <div class="modal_wrapper">
+          <p class="modal_title">🏆 당첨 통계 🏆</p>
+          <div class="modal_table">
+          <div class="modal_table_divider"></div>
+          <div class="modal_table_row">
+          <p class="modal_table_cell">일치 갯수</p>
+                <p class="modal_table_cell">당첨금</p>
+                <p class="modal_table_cell">당첨 갯수</p>
+              </div>
+              <div class="modal_table_divider"></div>
+              <div class="modal_table_row">
+                <p class="modal_table_cell">3개</p>
+                <p class="modal_table_cell">5,000</p>
+                <p class="modal_table_cell">n개</p>
+                </div>
+              <div class="modal_table_divider"></div>
+              <div class="modal_table_row">
+                <p class="modal_table_cell">4개</p>
+                <p class="modal_table_cell">50,000</p>
+                <p class="modal_table_cell">n개</p>
+                </div>
+                <div class="modal_table_divider"></div>
+                <div class="modal_table_row">
+                <p class="modal_table_cell">5개</p>
+                <p class="modal_table_cell">1,500,000</p>
+                <p class="modal_table_cell">n개</p>
+              </div>
+              <div class="modal_table_divider"></div>
+              <div class="modal_table_row">
+              <p class="modal_table_cell">5개+보너스볼</p>
+              <p class="modal_table_cell">30,000,000</p>
+              <p class="modal_table_cell">n개</p>
+              </div>
+              <div class="modal_table_divider"></div>
+              <div class="modal_table_row">
+                <p class="modal_table_cell">6개</p>
+                <p class="modal_table_cell">2,000,000,000</p>
+                <p class="modal_table_cell">n개</p>
+              </div>
+            </div>
+            <p class="modal_profit_text">당신의 총 수익률은 %입니다.</p>
+            <button class="restart_button">다시 시작하기</button>
+            </div>
+            </div>
+    `;
+
+  const closeButton = document.getElementById("modalCloseButton");
+  closeButton.addEventListener("click", () => {
+    modalBackdrop.remove();
+  });
+
+  modalBackdrop.addEventListener("click", (e) => {
+    if (e.target === modalBackdrop) {
+      modalBackdrop.remove();
+    }
+  });
+
+  document.body.appendChild(modalBackdrop);
+});
